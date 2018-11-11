d30570900fd23104df02bd6595e32d3fedebfbd3
sikamedia <simonwantao@yahoo.com>
2018-11-11 01:26:23 +0100

    init front-end conversion to truffle drizzle.

--- Ethereum/client/src/App.js
+++ Ethereum/client/src/App.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReadString from "./ReadString";
 
 class App extends Component {
+  state = {loading: false, drizzleSate: null};
+
+  componentDidMount() {
+    const { drizzle } = this.props;
+
+    // subscribe to changes in the store
+    this.unsubscribe = drizzle.store.subscribe(() => {
+
+      // every time the store updates, grab the state from drizzle
+      const drizzleState = drizzle.store.getState();
+
+      // check to see if it's ready, if so, update local component state
+      if (drizzleState.drizzleStatus.initialized) {
+        this.setState({ loading: false, drizzleState });
+      }
+    });
+  }
+
+  compomentWillUnmount() {
+    this.unsubscribe();
+  }
+
   render() {
+    if (this.state.loading) return "Loading Drizzle...";
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <div id="root" className="App">
+        <head>
+          <title>Lego Invest</title>
+          <link href="css/bootstrap.min.css" rel="stylesheet"/>
+          <link href="css/style.css" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400" rel="stylesheet"/>
+        </head>
+        <nav className="navbar navbar-light bg-light">
+          <h2 className="navbar-brand" href="#">
+            <img src="img/BGzf.gif" width="90px" className="d-inline-block align-top"/>
+            Lego<span style="font-weight:300">invest</span>
+          </h2>
+        </nav>
+        <ReadString
+          drizzle={this.props.drizzle}
+          drizzleState={this.state.drizzleState}
+        />
+      </div> );
   }
 }
 
--- Ethereum/client/src/index.js
+++ Ethereum/client/src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Drizzle, generateStore} from "drizzle";
+import MyStringStore from "./contracts/MyStringStore.json";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//let drizzle know what contracts we what
+const options = { contracts: [MyStringStore] };
+
+//setup the drizzle store and drizzle
+const drizzleStore = generateStore(options);
+const drizzle = new Drizzle(options, drizzleStore);
+
+ReactDOM.render(<App drizzle={drizzle}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
